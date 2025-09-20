cde683a54a2a4ab74f93d159a78aad440b610403
sham edward <shamedward2@gmail.com>
2025-09-20 15:02:47 +0530

    v2 pause

--- lib/Core/utils/constant.dart
+++ lib/Core/utils/constant.dart
@@ -1,4 +1,4 @@
 class Constant{
   static const String minRequiredVersion = "1.0.0";
-  static const String versionApiEndpoint = "";
+  static const String versionApiEndpoint = "https://shamtech072005.github.io/electron_iq_cross/versionController.json";
 }
